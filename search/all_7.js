c71c557851a7016d18611b75232f6b8f9210a332
peterrum <peterrum@users.noreply.github.com>
2020-09-05 11:19:00 +0000

    deploy: 62883dd4caea2e7950504ed96308590e90f79b88

--- search/all_7.js
+++ search/all_7.js
@@ -1,6 +1,6 @@
 var searchData=
 [
   ['initialize_5fdof_5fvector_73',['initialize_dof_vector',['../classhyperdeal_1_1MatrixFree.html#ac1f8334310f290d9081d0c01cdcbdbb7',1,'hyperdeal::MatrixFree']]],
-  ['interpolate_74',['interpolate',['../classhyperdeal_1_1internal_1_1FEFaceNormalEvaluation.html#a3fb3ecb669b2b574d834a5bdba55a87e',1,'hyperdeal::internal::FEFaceNormalEvaluation']]],
+  ['interpolate_74',['interpolate',['../classhyperdeal_1_1internal_1_1FEFaceNormalEvaluation.html#a999e33ccaf0f2aab7d3b8ca86eb0b2fd',1,'hyperdeal::internal::FEFaceNormalEvaluation']]],
   ['is_5fecl_5fsupported_75',['is_ecl_supported',['../classhyperdeal_1_1MatrixFree.html#a4d5485c0d6daeda24e84063c9b05f27b',1,'hyperdeal::MatrixFree']]]
 ];
--- search/functions_4.js
+++ search/functions_4.js
@@ -4,5 +4,5 @@ var searchData=
   ['feevaluationbase_208',['FEEvaluationBase',['../classhyperdeal_1_1FEEvaluationBase.html#a6d35f16ac2d562306f2d7e769270714c',1,'hyperdeal::FEEvaluationBase']]],
   ['feevaluationinverse_209',['FEEvaluationInverse',['../classhyperdeal_1_1FEEvaluationInverse.html#ae8e2cfe6c7ec98f321e9d91f87c11845',1,'hyperdeal::FEEvaluationInverse']]],
   ['fefaceevaluation_210',['FEFaceEvaluation',['../classhyperdeal_1_1FEFaceEvaluation.html#a50679d5d86bd2f2a1a33f2b332832019',1,'hyperdeal::FEFaceEvaluation']]],
-  ['fefacenormalevaluation_211',['FEFaceNormalEvaluation',['../classhyperdeal_1_1internal_1_1FEFaceNormalEvaluation.html#a89bf35fdbb4bd5a5e389705c8cd30cf2',1,'hyperdeal::internal::FEFaceNormalEvaluation']]]
+  ['fefacenormalevaluation_211',['FEFaceNormalEvaluation',['../classhyperdeal_1_1internal_1_1FEFaceNormalEvaluation.html#a79c9f111fa46ef388b87bf392bfbd8b0',1,'hyperdeal::internal::FEFaceNormalEvaluation']]]
 ];
