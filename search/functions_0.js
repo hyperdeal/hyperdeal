ceff10c4d5bead71b37eb38b03e7abb36118078b
peterrum <peterrum@users.noreply.github.com>
2020-10-01 09:01:43 +0000

    deploy: 3756b85879eff3877d8dd65c4ec97cc296215c30

--- search/functions_0.js
+++ search/functions_0.js
@@ -4,6 +4,6 @@ var searchData=
   ['add_5fparameters_191',['add_parameters',['../structhyperdeal_1_1LowStorageRungeKuttaIntegratorParamters.html#acc8bd4bbc06fbb8c3ed6b6afe808401e',1,'hyperdeal::LowStorageRungeKuttaIntegratorParamters']]],
   ['additionaldata_192',['AdditionalData',['../structhyperdeal_1_1MatrixFree_1_1AdditionalData.html#ab758fb0da8bd384c5f05210c65f2715e',1,'hyperdeal::MatrixFree::AdditionalData']]],
   ['advectionoperation_193',['AdvectionOperation',['../classhyperdeal_1_1advection_1_1AdvectionOperation.html#a6ad3d0ce9726a29413071b60c722dc77',1,'hyperdeal::advection::AdvectionOperation']]],
-  ['apply_194',['apply',['../classhyperdeal_1_1advection_1_1AdvectionOperation.html#a63e10e5610390efdffae388d568136ea',1,'hyperdeal::advection::AdvectionOperation']]],
+  ['apply_194',['apply',['../classhyperdeal_1_1advection_1_1AdvectionOperation.html#a780530cdb64996196844a59c4fc81871',1,'hyperdeal::advection::AdvectionOperation']]],
   ['are_5fghost_5ffaces_5fsupported_195',['are_ghost_faces_supported',['../classhyperdeal_1_1MatrixFree.html#aa1938b2ce8c090417e6560c2c617d40c',1,'hyperdeal::MatrixFree']]]
 ];
