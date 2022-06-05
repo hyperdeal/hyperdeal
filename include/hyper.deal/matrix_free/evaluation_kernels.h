ee1c4a1c92d48ad524ce13bf1d8f402163d5ac86
Peter Munch <peterrmuench@gmail.com>
2022-06-05 20:54:18 +0200

    Fix another case

--- include/hyper.deal/matrix_free/evaluation_kernels.h
+++ include/hyper.deal/matrix_free/evaluation_kernels.h
@@ -172,8 +172,28 @@ namespace hyperdeal
           }
         else if ((dim_v == 3) && (face_direction == (1 + dim_x)))
           {
-            for (int i = 0; i < dealii::Utilities::pow(n_rows, dim - 1); ++i)
-              out[i] = -1.0;
+            for (int i3 = 0; i3 < n_rows; ++i3)
+              for (int i2 = 0; i2 < n_rows; ++i2)
+                for (int i1 = 0;
+                     i1 < dealii::Utilities::pow<unsigned int>(n_rows, dim_x);
+                     ++i1)
+                  {
+                    const unsigned out_index =
+                      i1 +
+                      i3 * dealii::Utilities::pow<unsigned int>(n_rows, dim_x) +
+                      i2 *
+                        dealii::Utilities::pow<unsigned int>(n_rows, dim_x + 1);
+                    const unsigned in_index =
+                      i1 +
+                      i2 * dealii::Utilities::pow<unsigned int>(n_rows, dim_x) +
+                      i3 *
+                        dealii::Utilities::pow<unsigned int>(n_rows, dim_x + 2);
+
+                    apply_face_1D<face_direction,
+                                  contract_onto_face,
+                                  add,
+                                  max_derivative>(in, out, in_index, out_index);
+                  }
           }
         else // lex
           {
