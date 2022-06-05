b8fe7ea0c09a0b2357a7195a8efbb8ad5e332a45
Peter Munch <peterrmuench@gmail.com>
2022-06-05 21:04:09 +0200

    Warning

--- include/hyper.deal/matrix_free/evaluation_kernels.h
+++ include/hyper.deal/matrix_free/evaluation_kernels.h
@@ -150,9 +150,7 @@ namespace hyperdeal
 
         if ((dim_x == 3) && (face_direction == 1))
           {
-            for (int i3 = 0;
-                 i3 < dealii::Utilities::pow<unsigned int>(n_rows, dim_v);
-                 ++i3)
+            for (int i3 = 0; i3 < dealii::Utilities::pow(n_rows, dim_v); ++i3)
               for (int i2 = 0; i2 < n_rows; ++i2)
                 for (int i1 = 0; i1 < n_rows; ++i1)
                   {
@@ -174,8 +172,7 @@ namespace hyperdeal
           {
             for (int i3 = 0; i3 < n_rows; ++i3)
               for (int i2 = 0; i2 < n_rows; ++i2)
-                for (int i1 = 0;
-                     i1 < dealii::Utilities::pow<unsigned int>(n_rows, dim_x);
+                for (int i1 = 0; i1 < dealii::Utilities::pow(n_rows, dim_x);
                      ++i1)
                   {
                     const unsigned out_index =
