0cee82fc4229abc8dd61f7313706dd16912243a3
peterrum <peterrum@users.noreply.github.com>
2023-06-14 14:56:04 +0000

    deploy: 635470dfbc8be5e84c507318f2cf33916eee13ae

--- search/all_5.js
+++ search/all_5.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['face_5fall_0',['face_all',['../structhyperdeal_1_1internal_1_1MatrixFreeFunctions_1_1FaceInfo.html#ad6f45d2a38b58c9b47f2be99a26a646c',1,'hyperdeal::internal::MatrixFreeFunctions::FaceInfo']]],
-  ['face_5forientations_1',['face_orientations',['../structhyperdeal_1_1internal_1_1MatrixFreeFunctions_1_1ShapeInfo.html#a060a07a531fdc5e9fbcf7f3f88ea3004',1,'hyperdeal::internal::MatrixFreeFunctions::ShapeInfo::face_orientations()'],['../structhyperdeal_1_1internal_1_1MatrixFreeFunctions_1_1FaceInfo.html#a6b704fb0d24951ee1ea9375e492b2ac9',1,'hyperdeal::internal::MatrixFreeFunctions::FaceInfo::face_orientations()']]],
+  ['face_5forientations_1',['face_orientations',['../structhyperdeal_1_1internal_1_1MatrixFreeFunctions_1_1ShapeInfo.html#a060a07a531fdc5e9fbcf7f3f88ea3004',1,'hyperdeal::internal::MatrixFreeFunctions::ShapeInfo::face_orientations'],['../structhyperdeal_1_1internal_1_1MatrixFreeFunctions_1_1FaceInfo.html#a6b704fb0d24951ee1ea9375e492b2ac9',1,'hyperdeal::internal::MatrixFreeFunctions::FaceInfo::face_orientations']]],
   ['face_5fto_5fcell_5findex_5fnodal_2',['face_to_cell_index_nodal',['../structhyperdeal_1_1internal_1_1MatrixFreeFunctions_1_1ShapeInfo.html#a75c8624915b31648729af60dc4be52e2',1,'hyperdeal::internal::MatrixFreeFunctions::ShapeInfo']]],
   ['face_5ftype_3',['face_type',['../structhyperdeal_1_1internal_1_1MatrixFreeFunctions_1_1FaceInfo.html#a504fa8cd82145b34fe01cc9c7e0e1fb7',1,'hyperdeal::internal::MatrixFreeFunctions::FaceInfo']]],
   ['faceinfo_4',['FaceInfo',['../structhyperdeal_1_1internal_1_1FaceInfo.html',1,'hyperdeal::internal::FaceInfo'],['../structhyperdeal_1_1internal_1_1MatrixFreeFunctions_1_1FaceInfo.html',1,'hyperdeal::internal::MatrixFreeFunctions::FaceInfo']]],
